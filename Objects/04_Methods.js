b69bd0a98c194e479a4c390b1bea865abf79f5a6
pekarskiy <andrewpeka@mail.ru>
2015-02-16 15:13:51 +0300

    comment to js methods

--- Objects/04_Methods.js
+++ Objects/04_Methods.js
@@ -1,3 +1,6 @@
+// Объект может содержать методы.
+// Например, метод person.fullName возвращает имя и фамилию,
+// объекта person, разделенные пробелом.
 var person = {
     firstName: "Tom",
     lastName: "Watson",
