504fd9d07dad65b9536f11614492706e74f7b82b
pekarskiy <andrewpeka@mail.ru>
2015-02-18 18:59:15 +0300

    Prototype example has been added

---
+++ Objects/06_Prototype.js
@@ -0,0 +1,12 @@
+function Cat(name, breed) {
+	this.name = name;
+	this.breed = breed;
+}
+
+var cheshire = new Cat("Cheshire Cat", "British Shorthair");
+var gary = new Cat("Gary", "Domestic Shorthair");
+
+Cat.prototype.meow = function() {
+	return this.name + " Meow!!!";
+}
+
